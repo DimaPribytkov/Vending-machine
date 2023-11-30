d3cf98060e2daf2c38223ababa9a58a6b11e971a
ДимаПрибытков <d.pribytkov93@mail.ru>
2023-11-30 21:39:34 +0300

    Написал полную программу

--- Veging/src/Main.java
+++ Veging/src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int twixPrice = 100;
+        int marsPrice = 70;
+        int snickersPrice = 50;
+        int bonAqaPrice = 30;
+
+        boolean isCanBuy = false;
+
+        System.out.println("\t\t\t *****Добро пожаловать!*****");
+        System.out.println("\t\t ****\tВнесите нужную сумму!\t****");
+        int moneyAmount = new Scanner(System.in).nextInt();
+        if (moneyAmount >= twixPrice) {
+            System.out.println("Вы можете купить твикс!");
+            isCanBuy = true;
+        }
+        if (moneyAmount >= marsPrice) {
+            System.out.println("Вы можете купить марс!");
+            isCanBuy = true;
+        }
+        if (moneyAmount >= snickersPrice) {
+            System.out.println("Вы можете купить сникерс!");
+            isCanBuy = true;
+        }
+        if (moneyAmount >= bonAqaPrice) {
+            System.out.println("Вы можете купить бонАква!");
+            isCanBuy = true;
+        }
+        if (isCanBuy == false){
+            System.out.println("У вас недостаточная сумма денег...:-(");
+        }
 
     }
+
 }
